08afbb00c4bd5cd1b06c3209dcf7421c8a293836
MrDecter <ghoillus@gmail.com>
2024-01-19 09:30:11 +0300

    -Добавлены фун-ии получения чисел. -Обработка результата
    Проблемы:
    При вводе более 2 значений, выводит принт 2 раза

--- src/Main.java
+++ src/Main.java
@@ -9,44 +9,81 @@ public class Main {
         String input = str.next();
         System.out.println(calc(input));
     }
-                                            //Класс каолькулятора
+                                            //Класс калькулятора
     public static String calc(String input){
-        String x = "";
-        String y = "";
+        int x = Integer.parseInt(DivisionLine(input,0));
+        int y = Integer.parseInt(DivisionLine(input,1));;
         String oper = SearchOper(input);
-        String res = "";
-        return x;
+        int res = 0;
+        switch (oper) {
+            case "+" -> res = x + y;
+            case "-" -> res = x - y;
+            case "/" -> res = x / y;
+            case "*" -> res = x * y;
+        }
+        return Integer.toString(res);
     }
     static String SearchOper(String input){
         String oper = "";
-        if(input.indexOf( '+' )>0){
-            oper = "+";
-        }else if(input.indexOf( '-' )>0){
-            oper = "-";
-        }else if(input.indexOf( '/' )>0){
-            oper = "/";
-        }else if(input.indexOf( '*' )>0){
-            oper = "*";
-        }else{
-            System.out.println("Неверная операция!");
-        }
+            if(input.indexOf( '+' )>0){
+                oper = "+";
+            }else if(input.indexOf( '-' )>0){
+                oper = "-";
+            }else if(input.indexOf( '/' )>0){
+                oper = "/";
+            }else if(input.indexOf( '*' )>0){
+                oper = "*";
+            }else{
+                System.out.println("Неверная операция!");
+            }
         return oper;
     }
                                             //Деление строки
-    static String DivisionLine(String str){
-        String numbers ="";
+    static String DivisionLine(String str, int index){
+        String[] ind = {};
+        String numbers = "0";
         if(str.indexOf( '+' )>0){
-            numbers = Arrays.toString(str.split( "[+]"));
+            ind = str.split( "[+]");
+            if (NumElem(ind)){
+                numbers = ind[index];
+            }else{
+                System.out.println("Больше 2-х элементов!");
+            }
         }else if(str.indexOf( '-' )>0){
-            numbers = Arrays.toString(str.split( "[-]"));
+            ind = str.split( "[-]");
+            if (NumElem(ind)){
+                numbers = ind[index];
+            }else{
+                System.out.println("Больше 2-х элементов!");
+            }
         }else if(str.indexOf( '/' )>0){
-            numbers = Arrays.toString(str.split( "[/]"));
+            ind = str.split( "[/]");
+            if (NumElem(ind)){
+                numbers = ind[index];
+            }else{
+                System.out.println("Больше 2-х элементов!");
+            }
         }else if(str.indexOf( '*' )>0){
-            numbers = Arrays.toString(str.split( "[*]"));
+            ind = str.split( "[*]");
+            if (NumElem(ind)){
+                numbers = ind[index];
+            }else{
+                System.out.println("Больше 2-х элементов!");
+            }
         }else{
             System.out.println("Неверная операция!");
         }
         return numbers;
+    }
+                                            //Проверка кол-ва цифр
+    static boolean NumElem (String[] x){
+        boolean log;
+        if (x.length > 2){
+            log = false;
+        }else {
+            log = true;
+        }
+        return log;
     }
 
                                             //Проверка типа
@@ -69,31 +106,4 @@ public class Main {
         String[] Roman = {"I","II","III","IV","V","VI","VII","VIII","IX","X"};
         return 0;
     }
-
-
 }
-
-
-
-/*
-if(str.indexOf( '+' )>0){
-numbers = str.split( "[+]" );
-            if(numbers.length > 2){
-        System.out.println("Значений больше 2!");
-            }else {
-result = Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
-            }
-                    }else if(str.indexOf( '-' )>0){
-numbers = str.split( "[-]" );
-result = Integer.parseInt( numbers[0] ) - Integer.parseInt( numbers[1] );
-        }else if(str.indexOf( '/' )>0){
-numbers = str.split( "[/]" );
-result = Integer.parseInt( numbers[0] ) / Integer.parseInt( numbers[1] );
-        }else if(str.indexOf( '*' )>0){
-numbers = str.split( "[*]" );
-result = Integer.parseInt( numbers[0] ) * Integer.parseInt( numbers[1] );
-        }else{
-                System.out.println("Неверная операция!");
-        }
-
- */
